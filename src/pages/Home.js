904bb05deaee00715bd37bfefa2ec2ef92d4bd69
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2025-01-22 14:49:14 +0200

    Pages

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// import react from "react";
+// import './Home.css';
+
+const Home = () =>{
+    return (
+        <div>Home?
+        <button >Button</button>
+ 
+        </div>
+    )
+}
+
+export default Home
